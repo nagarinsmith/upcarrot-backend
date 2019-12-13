cceea7953f5bfe17d2bfe50564ff70c6f5a1bcd5
Diana Mucilenita <mucilenita.diana@gmail.com>
2019-12-13 14:12:51 +0200

    first commit containing the springboot app with db configuration

---
+++ src/main/java/com/upcarrot/UpCarrot/UpCarrotApplication.java
@@ -0,0 +1,13 @@
+package com.upcarrot.UpCarrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpCarrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpCarrotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upcarrot/UpCarrot/UpCarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upcarrot.UpCarrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpCarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
