6c0e7b2b5edfd431726e346c40e8ae97c9616958
Diana Mucilenita <mucilenita.diana@gmail.com>
2020-01-12 22:08:47 +0200

    - remove entity Test with along with its repo,service and controller

--- src/main/java/com/upcarrot/UpCarrot/Repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.upcarrot.UpCarrot.Repository;
-
-import com.upcarrot.UpCarrot.Model.Test;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends BaseRepository<Test> {
-}
