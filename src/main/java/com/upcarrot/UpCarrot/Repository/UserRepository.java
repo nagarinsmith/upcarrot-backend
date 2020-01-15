020987fc1fe8f4837f207c2b72dbb1ad4624bbb6
obp3ter <15703843+obp3ter@users.noreply.github.com>
2020-01-15 22:32:14 +0200

    Auth User model User repo User service

---
+++ src/main/java/com/upcarrot/UpCarrot/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.upcarrot.UpCarrot.Repository;
+
+import com.upcarrot.UpCarrot.Model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends BaseRepository<User> {
+}
